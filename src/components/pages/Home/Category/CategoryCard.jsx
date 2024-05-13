c7ed070046a2a1f89e4b45d56a91e9f53b181019
the-user01 <sakibcom02@gmail.com>
2024-05-13 21:51:16 +0600

    Books Category component created

---
+++ src/components/pages/Home/Category/CategoryCard.jsx
@@ -0,0 +1,10 @@
+
+const CategoryCard = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default CategoryCard;
---
+++ src/components/pages/Home/Category/ShowCategory.jsx
@@ -0,0 +1,10 @@
+
+const ShowCategory = () => {
+    return (
+        <div>
+            show ShowCategory
+        </div>
+    );
+};
+
+export default ShowCategory;
